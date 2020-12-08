1568fc3dc02e4aa7e9f139c46471e020e4af8ae4
Felipe Thadeu <felipet65@gmail.com>
2020-12-08 16:00:55 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    access_key = "AKIAZC5L3CMOHMJK4IXL"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "us-east-1"
 }
